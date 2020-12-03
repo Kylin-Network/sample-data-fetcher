4149e6a82d3a94c6f30a0ddc03c5113ec09a96c5
kuleleyokn42 <kuleleyokn@protonmail.com>
2020-12-03 08:28:31 +0800

    Add API to return avaliable call list

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate pretty_env_logger;
 
 use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::collections::BTreeMap;
 use std::env;
 
@@ -10,13 +11,28 @@ type KylinNetworkAPI = kylin_network_api::KylinNetworkAPI;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct RpcRequest {
-    name: String,
+    api_name: String,
+}
+
+async fn api_list() -> Result<HttpResponse, Error> {
+    let raw_content = r#"[
+              "liquidation_order_list",
+              "bitmex_perpetual_contract_rate",
+              "bitmex_large_order_list",
+              "bitfinex_holdings_minutes"
+        ]"#;
+    let json_content: Value = serde_json::from_str(raw_content)?;
+    println!("Response: {:?}", json_content);
+    println!("============================================================");
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(json_content))
 }
 
 async fn rpc_handler(req: web::Json<RpcRequest>) -> Result<HttpResponse, Error> {
     println!("req: {:?}", req);
 
-    let rpc_fn_name = req.name.as_str();
+    let rpc_fn_name = req.api_name.as_str();
     let resp: String;
 
     let api_key = match env::var("KYLIN_API_KEY") {
@@ -74,6 +90,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             // enable logger
             .wrap(middleware::Logger::default())
+            .service(web::resource("/api_list").route(web::get().to(api_list)))
             .service(web::resource("/").route(web::post().to(rpc_handler)))
     })
     .bind("127.0.0.1:8080")?
