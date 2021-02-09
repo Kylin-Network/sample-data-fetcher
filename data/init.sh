7a7c6c33e6fc148c87d1966ae3ce791f4f4ad352
kuleleyokn42 <kuleleyokn@protonmail.com>
2021-02-09 21:57:31 +0800

    Update init script

---
+++ data/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+
+sleep 30
+
+curl -H 'Content-Type: application/json' \
+    -XPUT --data "@es_index.json" \
+    ${ES_HOST}/${ES_INDEX_NAME}
+
+curl -XPOST -H "kbn-xsrf: true" \
+    --form file="@kibana.ndjson" \
+    ${KIBANA_HOST}/api/saved_objects/_import?createNewCopies=true
+
--- data/init_es_index.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -H 'Content-Type: application/json' \
-    -XPUT --data "@es_index.json" \
-    http://localhost:9200/kylin_access_tracking
