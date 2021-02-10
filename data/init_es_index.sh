ac20907f0a4a5799a11541d646cc1218fef00e78
kuleleyokn42 <kuleleyokn@protonmail.com>
2021-02-10 22:45:16 +0800

    Fix delete index error

--- data/init_es_index.sh
+++ data/init_es_index.sh
@@ -3,8 +3,9 @@
 set -x
 
 echo "Delete es index and recreate it"
-curl -H 'Content-Type: application/json' 
+curl -H 'Content-Type: application/json' \
     -XDELETE ${ES_HOST}/${ES_INDEX_NAME}
+
 curl -H 'Content-Type: application/json' \
     -XPUT --data "@es_index.json" \
     ${ES_HOST}/${ES_INDEX_NAME}
