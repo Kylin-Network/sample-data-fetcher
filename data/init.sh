66be1929539b6a15993fd73ac562146c847b722f
kuleleyokn42 <kuleleyokn@protonmail.com>
2021-02-10 20:45:52 +0800

    Fix es index mapping error

--- data/init.sh
+++ data/init.sh
@@ -2,11 +2,14 @@
 
 set -x
 
+echo "Delete es index and recreate it"
+curl -H 'Content-Type: application/json' 
+    -XDELETE ${ES_HOST}/${ES_INDEX_NAME}
 curl -H 'Content-Type: application/json' \
     -XPUT --data "@es_index.json" \
     ${ES_HOST}/${ES_INDEX_NAME}
 
 curl -XPOST -H "kbn-xsrf: true" \
     --form file="@kibana.ndjson" \
-    ${KIBANA_HOST}/api/saved_objects/_import?createNewCopies=true
+    ${KIBANA_HOST}/api/saved_objects/_import
 
